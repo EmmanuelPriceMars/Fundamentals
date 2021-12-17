d768741e0a0bf9ab9d6d3f53d2f132d8e6a17265
dmars <dmars@cnm.edu>
2021-12-16 17:59:04 -0700

    Open hears.exe when listening

---
+++ Variables-and-data.js
@@ -0,0 +1,17 @@
+/*
+* variables, data types, and typing
+ */
+
+
+let obj = {
+    key1: "value",
+    key2: 4,
+    boolean: true,
+    obj2: {
+        obj2key1: "internal object value",
+
+    }
+}
+
+console.log(obj.obj2.obj2key1);
+
